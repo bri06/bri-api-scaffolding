f972aa97d121ecd56427ae7505910ce81165aea3
Briam Martinez <briammartinez@gmail.com>
2020-08-11 08:08:06 +0200

    chore: added cors package

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
 const expressJSDocSwagger = require('express-jsdoc-swagger');
 
 const config = require('../config.js');
@@ -9,6 +10,7 @@ const songs = require('./components/Songs/network');
 const app = express();
 
 expressJSDocSwagger(app)(config.api.swaggerOptions);
+app.use(cors());
 app.use(helmet());
 app.use(morgan('tiny', { skip: () => process.env.NODE_ENV === 'test' }));
 app.use('/api/v1/songs', songs);
