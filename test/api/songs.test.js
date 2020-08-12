e55c2a951c37942a09f3c5d12781192e04f04aa4
Briam Martinez <briammartinez@gmail.com>
2020-08-12 16:18:18 +0200

    test: added remove endpoint test

--- test/api/songs.test.js
+++ test/api/songs.test.js
@@ -19,4 +19,13 @@ describe('Components/Songs', () => {
   it('responds with error when sond id does not exists', () => request(app)
     .get('/api/v1/songs/1000')
     .expect(404));
+  it('Should delete a song correctly', () => {
+    const deletedSong = {
+      id: 1, title: 'Song 1', artist: 'Artist 1', year: '2020',
+    };
+    return request(app)
+      .delete('/api/v1/songs/1')
+      .expect(200)
+      .then(response => expect(response.body).toEqual(deletedSong));
+  });
 });
